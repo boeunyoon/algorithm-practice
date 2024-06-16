ddfd6fd64cdb928a370018aaf0304812a00cff0c
boeunyoon <100947379+boeunyoon@users.noreply.github.com>
2024-06-16 13:56:34 +0900

    [Gold V] Title: 탑, Time: 680 ms, Memory: 140188 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -5,44 +5,47 @@ import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
+	static int N; 
 	public static void main(String[] args) throws IOException {
-		//자신의 기준 왼쪽의 자신보다 보다 높은 탑의 위치를 반환하라
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
 		StringBuilder sb = new StringBuilder();
-		int N = Integer.parseInt(st.nextToken());
-		st = new StringTokenizer(br.readLine());		
-		Stack<int[]> stack = new Stack<>();
-		
-		int num = 0;
-		int index = 0;
+		Stack<Point> stack = new Stack<Point>();
+		st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < N; i++) {
-			int[] arr = new int[] {Integer.parseInt(st.nextToken()), i+1};
-			num = arr[0];
+			int h = Integer.parseInt(st.nextToken());
 			if(stack.isEmpty()) {
-				sb.append(0).append(" ");
+				sb.append("0 ");
+				stack.push(new Point(i, h));
 			}else {
 				while(true) {
-					int[] data = stack.peek();
-					int id = data[1];
-					int tall = data[0];
-					if(tall < num) {
-						stack.pop();
-					}else {
-						sb.append(id).append(" ");
-						break;
-					}
 					if(stack.isEmpty()) {
-						sb.append(0).append(" ");
+						sb.append("0 ");
+						stack.push(new Point(i, h));
 						break;
 					}
-					
+					Point p = stack.peek();
+					if(p.height > h) {
+						sb.append(p.idx + 1 + " ");
+						stack.push(new Point(i, h));
+						break;
+					}else {
+						stack.pop();
+					}
 				}
 			}
-			stack.push(arr);
-			
-			
 		}
-		System.out.println(sb);
+		System.out.println(sb.toString());
+	}
+	static class Point{
+		int idx, height;
+
+		public Point(int idx, int height) {
+			super();
+			this.idx = idx;
+			this.height = height;
+		}
+		
 	}
 }
