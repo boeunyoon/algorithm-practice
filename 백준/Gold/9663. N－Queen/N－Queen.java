5d610c6475fcfdb996bfc1ed504042772e72a907
boeunyoon <100947379+boeunyoon@users.noreply.github.com>
2024-06-20 22:19:44 +0900

    [Gold IV] Title: N-Queen, Time: 2588 ms, Memory: 12232 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -5,36 +5,33 @@ import java.util.StringTokenizer;
 
 public class Main {
 	static int N, ans;
-	static int[][] map;
-	static boolean[] visited1, visited2, visited3;
-//	static int[] di = {-1, 0, 1, 0, 1, -1, 1}
+	static boolean[] visited, visited1, visited2;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
-		map = new int[N][N];
-		visited1 = new boolean[40];
-		visited2 = new boolean[40];
-		visited3 = new boolean[40];
+		visited = new boolean[N];
+		visited1 = new boolean[N + N]; //좌하, 우상 대각
+		visited2 = new boolean[N + N]; //좌상, 우하 대각
 		ans = 0;
-		dfs(0);
+		rec(0);
 		System.out.println(ans);
 	}
-	static void dfs(int count) {
-		if(count == N) {
+	static void rec(int count) {
+		if(count == N) { //종료 조건
 			ans++;
 			return;
 		}
+		//가능한 부분을 체크한다. 행에는 퀀이 무조건 1개 -> 행은 생각할 필요 없음, 열과 대각선만 체크하면된다.
 		for(int i = 0; i < N; i++) {
-			if(visited1[i] || visited2[i + count] || visited3[count - i + N - 1]) continue;
-			visited1[i] = true;
-			visited2[i + count] = true;
-			visited3[count - i + N - 1] = true;
-			dfs(count + 1);
-			visited1[i] = false;
-			visited2[i + count] = false;
-			visited3[count - i + N - 1] = false;
-	
+			if(visited[i] || visited1[i + count] || visited2[count - i + N - 1]) continue;
+			visited[i] = true;
+			visited1[i + count] = true;
+			visited2[count - i + N - 1] = true;
+			rec(count + 1);
+			visited[i] = false;
+			visited1[i + count] = false;
+			visited2[count - i + N - 1] = false;
 		}
 	}
 }
